b9cb5bebadf25bccf5c5afa8841b9d5add113ee5
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-07-16 14:51:13 -0400

    add the sigs and docs for the Constructors of the Transaction class

--- OneCoin/Tx.hpp
+++ OneCoin/Tx.hpp
@@ -20,12 +20,12 @@ namespace Tx {
      *  the hash of the output.
      * 
      *  Transactions have five additional data members:
-     *      - < b>epoch< /b> is the time that the Transaction was created in < i>unsigned long< /i> form.
-     *      - < b>author< /b> is the public key of the user that created and Transaction. This is in PEM format.
-     *      - < b>hash< b/> is the hash of the Transactions. The hash uses the all the data members, except the nonce.
-     *      - < b>nonce< b/> is used to calculate and verify the < i>Proof of Work< /i>. A nonce is considered valid if and only if
+     *      - <b>epoch</b> is the time that the Transaction was created in <i>unsigned long</i> form.
+     *      - <b>author</b> is the public key of the user that created and Transaction. This is in PEM format.
+     *      - <b>hash<b/> is the hash of the Transactions. The hash uses the all the data members, except the nonce.
+     *      - <b>nonce<b/> is used to calculate and verify the <i>Proof of Work</i>. A nonce is considered valid if and only if
      *         the hash of the nonce + the hash ends in at last four zeros. The nonce is calculated using a stochastic method.
-     *      - < b>signature< /b> is used to verify that a Transaction was created by the owner of the currency units being spent.
+     *      - <b>signature</b> is used to verify that a Transaction was created by the owner of the currency units being spent.
      *          The signature is the ECDSA signature of the hash when signed by the Transaction's author.
      */
     class Transaction {
@@ -45,6 +45,42 @@ namespace Tx {
             /* Transaction Outputs */
             std::string     output_user_key;
             std::size_t     output_hash;
+
+        public:
+
+            /**
+             * @brief The Transaction constructor creates a new Transaction object using the data provided by the user.
+             * This provided data includes the public key, the user to send the currency unit to, and the input info.
+             * .
+             * @param public_key_filepath   This is a string directing the constructor where to find the PEM file
+             * containing the <i>Public Key</i> of the Transaction's author. If the file cannot be found, a <i>runtime_error</i>
+             * is thrown.
+             * @param input_block   This is the hash of the block where the input token is located. The input token
+             * is the input to the Transaction, it directs the <i>Blockchain</i> where to find the currency units being spent.
+             * Warning: The validity of input_block is not checked when the constructor is being called, however an incorrect
+             * input_block will cause the Transaction to be null and void.
+             * @param input_tx  This is the hash of the Transaction where the input token is located. This directs the <i>Blockchain</i>
+             * where to look for the inputed currency unit.
+             * Warning: The validity of input_block is not checked when the constructor is being called, however an incorrect
+             * input_block will cause the Transaction to be null and void.
+             * @param reciever  This is the <i>public key</i> of the user to which the currency units are being sent to. The key
+             * should be a basic string, in PEM format. Be sure not to omit newline (\\n) characters when newlines are present in the PEM
+             * file.
+             */
+            Transaction(std::string public_key_filepath, std::size_t input_block, std::size_t input_tx, std::string reciever) {
+
+            }
+
+            /**
+             * @brief The Transaction constructor creates a new Transaction using the serialized JSON data from another Transaction object.
+             * The given JSON string should be valid and from another Transaction object.
+             * .
+             * @param json_data This is the JSON string to use to create the Transaction object. This input should be a valid JSON string
+             * for the Transaction class.
+             */
+            Transaction(std::string json_data) {
+
+            }
 
     };
 }
