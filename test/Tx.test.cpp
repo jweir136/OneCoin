2474deab3f49360e0da0ab3db8d85e2ea39423f5
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-08-03 18:57:34 -0400

    remove all the signing stuff...it isn't seeming to work. I will try to experiment with it in another branch

--- test/Tx.test.cpp
+++ test/Tx.test.cpp
@@ -9,7 +9,8 @@ const std::string pub2 = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj
 const std::string pub3 = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEyQQr7BgHRtOB7SJTFbS7i06DX1lo\nQsKDuHIdOmL4z/frRzjdMUuowBH4FyNWDkH51HSxIhZKgxotYR35JAWPPw==\n-----END PUBLIC KEY-----";
 const std::string pub_keys[3] = { pub1, pub2, pub3 };
 const std::string priv_keys[3] = {
-    "sample_private_keys/priv1.pem", "sample_private_keys/priv2.pem", "sample_private_keys/priv3.pem"
+    "/Users/jacobweir/projects/cpp/OneCoin/test/sample_private_keys/priv1.pem",
+    "test/sample_private_keys/priv2.pem", "test/sample_private_keys/priv3.pem"
 };
 
 TEST_CASE("tx-serialization", "[]") {
