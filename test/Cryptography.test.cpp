654f100d57040fcddf540d2086ca8b55f87339b2
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-08-03 19:08:52 -0400

    add the signing-with-string test

--- test/Cryptography.test.cpp
+++ test/Cryptography.test.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <string>
+#include <iostream>
 
 #include <OneCoin/Cryptography.hpp>
 
@@ -12,4 +13,13 @@ TEST_CASE("signing", "[]") {
     std::string sig = ECDSA::sign("priv.pem", test);
 
     REQUIRE(ECDSA::verify("pub.pem", test, sig));
+};
+
+TEST_CASE("signing-with-string", "[]") {
+    std::string test = "abc";
+    std::string reciever = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEuhPwLwTe2MEZwYFIlBCLsxaZOlER\nNICe4W4NyCzSIVvwPyQVl+7LnpA/pjoZxFozrrDu1eIgKRTBm3cXhUtKpA==\n-----END PUBLIC KEY-----";
+
+    std::string sig = ECDSA::sign("priv.pem", test);
+    bool passed = ECDSA::verify_from_string(reciever, test, sig);
+    REQUIRE(passed);
 };
