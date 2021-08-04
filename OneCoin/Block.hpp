d74664750960ede9f525afc124b12733c2e802d1
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2021-08-03 22:02:03 -0400

    add construtor docs

--- OneCoin/Block.hpp
+++ OneCoin/Block.hpp
@@ -6,6 +6,15 @@
 
 using namespace nlohmann;
 
+/**
+ * @brief A Block object is the entire foundation of the most fundamental data structure in bitcoin,
+ * the Blockchain. A Block object is simply used to group and store Transaction objects and store them
+ * on the Blockchain.
+ * 
+ * All Blocks have three fields: the blocks, the hash, and the size. The blocks field is just a list
+ * of the serialized Transaction objects. The hash is the hash of all the Transaction's hashes appended
+ * together. The size is just the number of Transactions in the Block.
+ */
 class Block {
     private:
         json            blocks;
@@ -14,7 +23,10 @@ class Block {
         json            json_data;
 
     public:
-
+        /**
+         * @brief The constructor is used to make an empty Block object. An empty Block has a hash of
+         * zero.
+         */
         Block() {
             this->blocks = {};
             this->hash = 0;
@@ -25,6 +37,11 @@ class Block {
             this->json_data["hash"] = this->hash;
         }
 
+        /**
+         * @brief The constructor takes a serialized Block object and creates a new object. The serialized
+         * string is expected to be in JSON format, and from another Block object.
+         * @param json_data This is the JSON string another serialized Block.
+         */
         Block(std::string json_data) {
             this->json_data = json::parse(json_data);
             
